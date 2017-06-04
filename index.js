264bb1da59d9573e61a47d2fe6442283f90d4a31
Greg Lamp <glamp@Yhats-MacBook-Air.local>
2017-06-04 10:42:19 -0400

    fixing args. adding in checks

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const fs = require('fs');
 const semver = require('semver');
 const pkg = require('./package.json');
-var { docopt } = require('docopt');
+const { docopt } = require('docopt');
 
 const doc = `
 Usage:
-  bumper <package.json> <release>
+  bumper <manifest.json> [--release=<type>]
   bumper -h | --help | --version
+
+Options:
+  -r --release	Release type (major, minor, patch, premajor, preminor, prepatch, or prerelease) [default: minor].
 `
 
 var args = docopt(doc, { version: pkg.version });
+var manifestFile = args['<manifest.json>'];
 
-var package = JSON.parse(fs.readFileSync(args['<package.json>']));
+if (! fs.existsSync(manifestFile)) {
+  process.stderr.write(`manfiest file does not exist: '${maifestFile}'`);
+  process.exit(1);
+}
+
+var package;
+try {
+  package = JSON.parse(fs.readFileSync(manifestFile));
+} catch(e) {
+  process.stderr.write(`manfiest file is not valid JSON: '${e}'`);
+  process.exit(1);
+}
+
 var currentVersion = package.version;
-var release = args['<release>'];
+var release = args['--release'] || 'minor';
 
 var newVersion = semver.inc(currentVersion, release);
-package.version = newVersion;
 
-fs.writeFileSync(args['<package.json>'], JSON.stringify(package, null, 2));
+if (newVersion==null) {
+  process.stderr.write(`could not bump version. probably invalid version in your manifest.json file. the current value is ${currentVersion}`);
+  process.exit(1);
+}
+
+package.version = newVersion;
+fs.writeFileSync(manifestFile, JSON.stringify(package, null, 2));
